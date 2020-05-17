3145d230627e7fa92a697c73077c78211de46292
Shreshth Srivastava <shreshth28srivastava@gmail.com>
2020-05-17 11:28:40 +0530

    Added ListView Widget Support

--- app/src/main/java/com/example/masterflow/IngredientAppWidgetProvider.java
+++ app/src/main/java/com/example/masterflow/IngredientAppWidgetProvider.java
@@ -5,6 +5,9 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
 import android.widget.RemoteViews;
 
 public class IngredientAppWidgetProvider extends AppWidgetProvider {
@@ -15,12 +18,43 @@ public class IngredientAppWidgetProvider extends AppWidgetProvider {
             Intent intent=new Intent(context,MainActivity.class);
             PendingIntent pendingIntent=PendingIntent.getActivity(context,0,intent,0);
 
-            RemoteViews views=new RemoteViews(context.getPackageName(),R.layout.ingredient_widget);
-            views.setOnClickPendingIntent(R.id.ingredient_btn,pendingIntent);
 
+
+            Intent serviceIntent = new Intent(context,IngredientWidgetService.class);
+            serviceIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,appWidgetId);
+            serviceIntent.setData(Uri.parse(serviceIntent.toUri(Intent.URI_INTENT_SCHEME)));
+            RemoteViews views=new RemoteViews(context.getPackageName(),R.layout.ingredient_widget);
+            views.setOnClickPendingIntent(R.id.ingredient_widget_btn,pendingIntent);
+            views.setOnClickPendingIntent(R.id.ingredient_widget_item_text,pendingIntent);
+            views.setRemoteAdapter(R.id.ingredient_widget_list_view,serviceIntent);
+            views.setEmptyView(R.id.ingredient_widget_list_view,R.id.ingredient_widget_empty_view);
+
+//            Bundle appWidgetOptions=appWidgetManager.getAppWidgetOptions(appWidgetId);
+            views.setViewVisibility(R.id.ingredient_widget_btn, View.VISIBLE);
             appWidgetManager.updateAppWidget(appWidgetId,views);
 
-
         }
+    }
+
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        RemoteViews views=new RemoteViews(context.getPackageName(),R.layout.ingredient_widget);
+        appWidgetManager.updateAppWidget(appWidgetId,views);
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        super.onDeleted(context, appWidgetIds);
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        super.onEnabled(context);
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        super.onDisabled(context);
     }
 }
---
+++ app/src/main/java/com/example/masterflow/IngredientWidgetService.java
@@ -0,0 +1,78 @@
+package com.example.masterflow;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
+
+import java.util.List;
+
+
+public class IngredientWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new IngredientWidgetItemFactory(getApplicationContext(),intent);
+    }
+
+    class IngredientWidgetItemFactory implements RemoteViewsFactory{
+
+        private Context context;
+        private int appWidgetId;
+        private List<String> dataList=MainActivity.mainList.get(RecipeListFragment.indexSteps).getIngredient();
+
+        IngredientWidgetItemFactory(Context context,Intent intent)
+        {
+            this.context=context;
+            this.appWidgetId=intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                    AppWidgetManager.INVALID_APPWIDGET_ID);
+        }
+
+        @Override
+        public void onCreate() {
+            //connect to database
+        }
+
+        @Override
+        public void onDataSetChanged() {
+            //close data source
+        }
+
+        @Override
+        public void onDestroy() {
+
+        }
+
+        @Override
+        public int getCount() {
+            return dataList.size();
+        }
+
+        @Override
+        public RemoteViews getViewAt(int position) {
+            RemoteViews views=new RemoteViews(context.getPackageName(),R.layout.ingredient_widget_item);
+            views.setTextViewText(R.id.ingredient_widget_item_text,dataList.get(position));
+            return views;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 1;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+    }
+}
